9285488ec5d7488098b5c2eaeeb63aba10ab1418
Bibek <bibek.thapahere@gmail.com>
2016-09-07 15:16:44 -0700

    Experinmented with the reading line line in file

--- src/com/example/dosterminal/program.java
+++ src/com/example/dosterminal/program.java
@@ -1,33 +1,29 @@
-
 package com.example.dosterminal;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
 
 public class program {
 
-   
     public static void main(String[] args) {
-        String fileName="C:/Users/home/desktop/java/sample.txt";
-        try
-        {
-        int i=0;
-       FileReader reader=new FileReader(fileName);
-        while((i=reader.read())!=-1)
-      {
-          System.out.print((char)i);
-      
-      }
-        reader.close();
-        }catch(IOException ioe)
-        {
+        String fileName = "C:/Users/home/desktop/java/sample.txt";
+        try {
+            InputStream is = new FileInputStream(fileName);
+            byte[] data=new byte[1024*10];
+            int i=0;
+           while((i=is.read(data))!=-1)
+           {
+               System.out.println(i);
+           } 
+           is.close();
+            
+           
+        } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
-        
+
         }
-  
-       
+
     }
-    
+
 }
