23860112e276979784f2a395f5f5d89b59915564
Bibek <bibek.thapahere@gmail.com>
2016-09-07 15:22:02 -0700

    Made the program to copy the same file

--- src/com/example/dosterminal/program.java
+++ src/com/example/dosterminal/program.java
@@ -1,8 +1,11 @@
 package com.example.dosterminal;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class program {
 
@@ -10,13 +13,15 @@ public class program {
         String fileName = "C:/Users/home/desktop/java/sample.txt";
         try {
             InputStream is = new FileInputStream(fileName);
+            OutputStream os=new FileOutputStream(fileName +"_copy.txt");
             byte[] data=new byte[1024*10];
             int i=0;
            while((i=is.read(data))!=-1)
            {
-               System.out.println(i);
+               os.write(data, 0, i);
            } 
            is.close();
+           os.close();
             
            
         } catch (IOException ioe) {
