b376d1694d7038c0389b9deb127b742014212f87
Bibek <bibek.thapahere@gmail.com>
2016-09-07 15:33:40 -0700

    Inserted the BufferReader to print in screen which was not previously possible

--- src/com/example/dosterminal/program.java
+++ src/com/example/dosterminal/program.java
@@ -1,8 +1,10 @@
 package com.example.dosterminal;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,16 +14,15 @@ public class program {
     public static void main(String[] args) {
         String fileName = "C:/Users/home/desktop/java/sample.txt";
         try {
-            InputStream is = new FileInputStream(fileName);
-            OutputStream os=new FileOutputStream(fileName +"_copy.txt");
-            byte[] data=new byte[1024*10];
-            int i=0;
-           while((i=is.read(data))!=-1)
-           {
-               os.write(data, 0, i);
-           } 
-           is.close();
-           os.close();
+            BufferedReader reader =new BufferedReader(new FileReader(fileName));
+            String line="";
+            while((line=reader.readLine())!=null)
+            {
+                System.out.println(line);
+            
+            }
+            reader.close();
+          
             
            
         } catch (IOException ioe) {
