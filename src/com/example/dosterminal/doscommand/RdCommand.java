61483c98caf877240fd40aa5600ccecd89c65a68
Bibek <bibek.thapahere@gmail.com>
2016-09-07 17:59:54 -0700

    Added the command Factory and supporting commands

---
+++ src/com/example/dosterminal/doscommand/RdCommand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.dosterminal.doscommand;
+
+/**
+ *
+ * @author HOME
+ */
+public class RdCommand extends DosCommand{
+
+    @Override
+    public void execute(String[] params) {
+        
+        if (params.length>1)
+        {
+        
+        }
+        else
+        {
+            System.out.println("The syntax of the command is incorrect");
+        
+        }
+        
+        
+    }
+    
+}
---
+++ src/com/example/dosterminal/doscommand/RenameCommand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.dosterminal.doscommand;
+
+/**
+ *
+ * @author HOME
+ */
+public class RenameCommand extends DosCommand{
+
+    @Override
+    public void execute(String[] params) {
+       
+// throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
