6b9cb5c1d6df1f4f06f04c7cd0bceee5f9dc2b6b
Bibek <bibek.thapahere@gmail.com>
2016-09-07 17:00:37 -0700

    Added the Scanner and made program run until user presses exit button

--- src/com/example/dosterminal/program.java
+++ src/com/example/dosterminal/program.java
@@ -7,16 +7,18 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import static java.lang.System.in;
 
 public class program {
 
     public static void main(String[] args) {
         String fileName = "C:/Users/home/desktop/java/sample.txt";
         try {
-            BufferedReader reader =new BufferedReader(new FileReader(fileName));
+            BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
             String line="";
-            while((line=reader.readLine())!=null)
+            while(!(line=reader.readLine()).equalsIgnoreCase("exit"))
             {
                 System.out.println(line);
             
