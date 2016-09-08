cf6583d9b9347058237376678936843fab3adc68
Bibek <bibek.thapahere@gmail.com>
2016-09-07 17:15:19 -0700

    Made the program to print with directory

--- src/com/example/dosterminal/program.java
+++ src/com/example/dosterminal/program.java
@@ -2,25 +2,25 @@ package com.example.dosterminal;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import static java.lang.System.in;
 
 public class program {
 
     public static void main(String[] args) {
         String fileName = "C:/Users/home/desktop/java/sample.txt";
+        
         try {
+            File pwd =new File(".");
+            String path = pwd.getAbsolutePath();
             BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
             String line="";
             while(!(line=reader.readLine()).equalsIgnoreCase("exit"))
             {
+                System.out.println(path+">"
+                        );
                 System.out.println(line);
+                
             
             }
             reader.close();
